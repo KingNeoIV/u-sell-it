7b7af654087878275a62f905c3bbb1d4c5451b29
KingNeoIV <michael.rios.nw@hotmail.com>
2026-04-28 14:38:02 -0500

    Update v1 API router to include transactions route

--- backend/app/api/v1/__init__.py
+++ backend/app/api/v1/__init__.py
@@ -3,19 +3,17 @@ from app.api.v1.routes import auth, users
 from .routes.listings import router as listings_router
 from .routes.images import router as images_router
 from .routes.categories import router as categories_router
+from .routes.transaction import router as transactions_router
 
-# Root API router for version v1.
-# This module aggregates and mounts all route modules under a single router.
+# Main API router for version v1
 api_router = APIRouter()
 
-# Core application routes.
-# Each module exposes its own router, which is included here.
+# Core routes
 api_router.include_router(auth.router)
 api_router.include_router(users.router)
 
-# Feature-specific routes with explicit prefixes and tags.
-# These routers are defined locally withing the v1 package.
+# Feature routes
 api_router.include_router(listings_router, prefix="/listings", tags=["listings"])
 api_router.include_router(images_router, prefix="/images", tags=["images"])
 api_router.include_router(categories_router, prefix="/categories", tags=["categories"])
-
+api_router.include_router(transactions_router, prefix="/transactions", tags=["transactions"])
